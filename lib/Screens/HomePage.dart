704e69c2e55539f740229f3836cf0d56e736eac8
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-10-07 17:51:36 +0530

    trying to add snake moving functionality

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,17 +15,41 @@ class _HomePageState extends State<HomePage> {
     final _width = MediaQuery.of(context).size.width;
     print((_width * 0.065).toInt());
 
-    // total number of rows to be displayed
-    int numberOfRows = (double.maxFinite).toInt();
-
     // total number of squares in each row
     int numberOfSquares = (_width * 0.065).toInt();
 
-    List<int> snake = [45, 65, 85, 105, 125];
+    // total number of rows to be displayed
+    int numberOfRows = 580;
+
+    List<int> snakePosition = [45, 65, 85, 105, 125];
 
     int food = 113;
 
     var boxColor;
+
+    String direction = 'down';
+    void _updateSnake() {
+      setState(() {
+        switch (direction) {
+          case 'down':
+            if (snakePosition.last + 20 > numberOfRows) {
+              snakePosition.add(snakePosition.last + 20 - numberOfRows);
+            } else {
+              snakePosition.add(snakePosition.last + 20);
+            }
+            break;
+        }
+        snakePosition.removeAt(0);
+      });
+    }
+
+    void _startGame() {
+      snakePosition = [45, 65, 85, 105, 125];
+      const duration = const Duration(milliseconds: 300);
+      Timer.periodic(duration, (Timer timer) {
+        _updateSnake();
+      });
+    }
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -37,23 +63,47 @@ class _HomePageState extends State<HomePage> {
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: numberOfSquares),
                   itemBuilder: (BuildContext context, int index) {
-                    if (snake.contains(index)) {
-                      boxColor = Colors.white;
-                    } else if (food == index) {
-                      boxColor = Colors.green[400];
-                    } else {
-                      boxColor = Colors.grey[900];
-                    }
+                    // if (snakePosition.contains(index)) {
+                    //   boxColor = Colors.white;
+                    // } else if (food == index) {
+                    //   boxColor = Colors.green[400];
+                    // } else {
+                    //   boxColor = Colors.grey[900];
+                    // }
 
-                    return Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(3.5)),
-                          color: boxColor,
+                    if (snakePosition.contains(index))
+                      return Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(3.5)),
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                    );
+                      );
+                    else if (food == index)
+                      return Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(3.5)),
+                            color: Colors.green[400],
+                          ),
+                        ),
+                      );
+                    else
+                      return Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(3.5)),
+                            color: Colors.grey[900],
+                          ),
+                        ),
+                      );
                   }),
             ),
             Padding(
@@ -62,10 +112,13 @@ class _HomePageState extends State<HomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Container(
-                    child: Text(
-                      'Start',
-                      style: TextStyle(color: Colors.white),
+                  GestureDetector(
+                    onTap: _startGame,
+                    child: Container(
+                      child: Text(
+                        'Start',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                   Container(
