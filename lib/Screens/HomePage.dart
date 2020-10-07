c967a84bb4386a05ae06e8b12a47b13492c70b97
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-10-07 16:16:21 +0530

    created basic layout of the game

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -9,18 +9,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final _height = MediaQuery.of(context).size.height -
-        MediaQuery.of(context).padding.top -
-        kToolbarHeight;
+    final _height = MediaQuery.of(context).size.height;
     final _width = MediaQuery.of(context).size.width;
+    print((_width * 0.065).toInt());
+
+    int numberOfRows = (double.maxFinite).toInt();
+    int numberOfSquares = (_width * 0.065).toInt();
 
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-          child: Text(
-        'This Works',
-        style: TextStyle(color: Colors.white),
-      )),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: numberOfRows,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: numberOfSquares),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(3.5)),
+                          color: Colors.grey[900],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 10.0, top: 20.0, right: 10.0, bottom: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      'Start',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      '@CreatedByArush',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
